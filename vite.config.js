dca7a8713489cefaa92347427853efd323300485
violachyu <pinckychyu@gmail.com>
2025-05-09 21:40:04 -0400

    Fix: gitpage config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@
 */
 export default {
   // Set the base directory for GitHub pages
-  base: process.env.NODE_ENV === 'production' ? '/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/violachyu' : '/',
   build: {
     outDir: './dist',
     sourcemap: true,
